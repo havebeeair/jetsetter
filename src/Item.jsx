dc6f68e6ceddbaef7e724cefa3e1135594a48cad
Luis Escalona <luiscescalona@gmail.com>
2021-09-09 12:54:58 -0300

    Correcciones componente Item

--- src/Item.jsx
+++ src/Item.jsx
@@ -2,11 +2,11 @@ import './App.css';
 
 function Item () {
     return (
-    <div className="item-container" >
-        <input type="checkbox" className="checkbox-item" ></input>
-        <div className="item-texto">Pantalon</div>
-        <button className="btn-remover-item">Remover</button>
-    </div>
+        <div className="item-container">
+            <input type="checkbox" className="checkbox-item"/>
+            <div className="item-texto">Pantalon</div>
+            <button className="btn-remover-item">Remover</button>
+        </div>
     )
 }
 
