4592fd525d1210d4eec7ed60ef7bff1e60c7a377
Daniel Alvarado <danieltrm1810@gmail.com>
2021-09-11 20:40:18 -0300

    Se agrega en utils funcion borrarElementoLista

--- src/utils.jsx
+++ src/utils.jsx
@@ -1,8 +1,17 @@
 export function obtenerItemsPorCategoria(lista, categoria, valor) {
-    const nuevaLista = lista.filter((item) => {
-      if (item[categoria] === valor) {
-        return item;
-      }
-    });
-    return nuevaLista;
-  }
+  const nuevaLista = lista.filter((item) => {
+    if (item[categoria] === valor) {
+      return item;
+    }
+  });
+  return nuevaLista;
+}
+
+export function borrarElementoLista(lista, id) {
+  const listaModificada = lista.filter((item) => {
+    if (item.id !== id) {
+      return item;
+    }
+  });
+  return listaModificada;
+}
